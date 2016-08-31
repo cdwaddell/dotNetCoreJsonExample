4c33d3d5d859aaa3e046b86eec5843943e37f5e8
Charles D Waddell <cdwaddell@outlook.com>
2016-08-30 21:33:41 -0400

    Added data and scaffolding of knockout

--- src/dotNetCoreJsonExample/Startup.cs
+++ src/dotNetCoreJsonExample/Startup.cs
@@ -10,25 +10,32 @@ using Microsoft.Extensions.Logging;
 
 namespace dotNetCoreJsonExample
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseStaticFiles();
-    }
-    }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+			var options = new DefaultFilesOptions();
+			options.DefaultFileNames.Clear();
+			options.DefaultFileNames.Add("index.html");
+			app.UseDefaultFiles(options);
+			app.UseStaticFiles();
+		}
+	}
 }

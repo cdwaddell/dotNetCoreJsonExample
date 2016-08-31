24e47ee78ee41cf298612970602b7a23b4134f36
Charles D Waddell <cdwaddell@outlook.com>
2016-08-30 23:36:01 -0400

    finalize and publish

---
+++ src/dotNetCoreJsonExample/Properties/PublishProfiles/comcdwaddell - Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'a2f5e16e-bd25-493f-aa34-d62a3b3f54c4'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
